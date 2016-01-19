f4f022bd36d1cecffc2092b1f2f95bcb1f6d1460
gregpriday <greg@siteorigin.com>
2016-01-19 11:50:51 +0200

    Start of new Github updater

--- siteorigin-installer.php
+++ siteorigin-installer.php
@@ -11,15 +11,15 @@ License URI: http://www.opensource.org/licenses/gpl-license.php
 */
 
 if( !defined( 'SITEORIGIN_INSTALLER_VERSION' ) ) {
-	define('SITEORIGIN_INSTALLER_VERSION', 'dev');
+	define('SITEORIGIN_INSTALLER_VERSION', '0.1');
 }
 
 require_once dirname( __FILE__ ) . '/inc/class-tgm-plugin-activation.php';
 require_once dirname( __FILE__ ) . '/siteorigin-installer-theme-admin.php';
 
 // Add WP Updates
-require_once dirname( __FILE__ ) . '/inc/wp-updates-plugin.php';
-new WPUpdatesPluginUpdater_1419( 'http://wp-updates.com/api/2/plugin', plugin_basename(__FILE__));
+require_once dirname( __FILE__ ) . '/inc/github-plugin-updater.php';
+new SiteOrigin_Installer_GitHub_Updater( __FILE__ );
 
 if( !class_exists('SiteOrigin_Installer') ) {
 	class SiteOrigin_Installer {
