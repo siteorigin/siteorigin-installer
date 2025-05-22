ff1e08d3d372a653dd17eff00510451a2dc05d8d
Andrew Misplon <amisplon@gmail.com>
2025-05-23 00:14:53 +0100

    Set release version in plugin files
    - Updated the `Version` field in the plugin header of `siteorigin-installer.php` to `1.0.3`.
    - Updated the `SITEORIGIN_INSTALLER_VERSION` constant to `1.0.3`.
    - Ensured the plugin reflects the current release version for accurate version management and updates.

--- siteorigin-installer.php
+++ siteorigin-installer.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: SiteOrigin Installer
 Description: Streamline your WordPress setup with SiteOrigin's essential plugins and compatible themes in one go.
-Version: dev
+Version: 1.0.3
 Requires at least: 4.7
 Tested up to: 6.8
 Requires PHP: 7.0.0
@@ -15,7 +15,7 @@ License URI: https://www.gnu.org/licenses/gpl-3.0.txt
 */
 
 if ( ! defined( 'SITEORIGIN_INSTALLER_VERSION' ) ) {
-	define( 'SITEORIGIN_INSTALLER_VERSION', 'dev' );
+	define( 'SITEORIGIN_INSTALLER_VERSION', '1.0.3' );
 	define( 'SITEORIGIN_INSTALLER_DIR', plugin_dir_path( __FILE__ ) );
 	define( 'SITEORIGIN_INSTALLER_URL', plugin_dir_url( __FILE__ ) );
 }
