f4d24cc3b0a0817d41ba53291e91091d24232cc6
Andrew Misplon <amisplon@gmail.com>
2025-05-28 21:53:55 +0100

    Test: Pull readme from move-updater-to-submodule branch and update updater

--- siteorigin-installer.php
+++ siteorigin-installer.php
@@ -66,7 +66,7 @@ if ( ! class_exists( 'SiteOrigin_Installer' ) ) {
 				if ( $is_standalone ) {
 					if ( file_exists( plugin_dir_path( __FILE__ ) . 'updater/updater.php' ) ) {
 						require_once plugin_dir_path( __FILE__ ) . 'updater/updater.php';
-						new SiteOrigin_Updater( __FILE__, 'siteorigin-installer', 'siteorigin-installer' );
+						new SiteOrigin_Updater( __FILE__, 'siteorigin-installer', 'siteorigin-installer', 'move-updater-to-submodule' );
 					}
 				}
 
